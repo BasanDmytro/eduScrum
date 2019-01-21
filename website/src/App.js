8519626befe3d09b8b05a7207eb996bab319c8a3
basandima <basandima@gmail.com>
2019-01-21 10:06:22 +0100

    Added create lane and cards

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Board from 'react-trello'
 import logo from './logo.svg';
+import NewCard from './trello.js'
 import './App.css';
 
 const data = {
@@ -10,22 +11,22 @@ const data = {
       title: 'Planned Tasks',
       label: '2/2',
       cards: [
-        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
+        {id: 'Card1', title: 'Write Blog', description: 'Can AI make', label: '30 mins'},
         {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
       ]
     },
     {
       id: 'lane2',
       title: 'Completed',
-      label: '0/0',
-      cards: []
+      label: '1/1',
+      cards: [{id: 'Card1', title: 'Write Blog', description: 'Can AI make 32', label: '30 mins'},]
     }
   ]
 };
 
 class App extends Component {
   render() {
-    return <Board data={data} />
+    return <NewCard />
   }
 }
 

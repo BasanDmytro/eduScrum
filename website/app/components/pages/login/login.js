028326f6f8270c5f5410627a275abd444a15222d
Anthony Lardy <lardy.anthony@hotmail.com>
2019-03-09 16:49:17 +0100

    Adding login view

--- website/app/components/pages/login/login.js
+++ website/app/components/pages/login/login.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { ReactTextField, validator } from 'react-textfield';
 
 class Login extends Component {
   render() {
     return (
       <div>
-        <button />
+        <button onClick={(e) => this.handleClick(e)}>
+            Se connecter
+        </button>
+        <ReactTextField
+        name="Identifiant"
+        type="text"
+        placeholder="Identifiant"
+        validators={[
+          {
+            message: "La longueur de l'identifiant doit être compris entre 4 et 12 charactères.",
+            validator: value => validator.length(value, { min: 4, max: 12 }),
+          },
+          {
+            message: "L'identifiant doit comporter seulement des lettres.",
+            validator: value => validator.isAlphanumeric(value),
+          },
+        ]}
+        successMessage="Cet identifiant est invalide"
+        />
+        <ReactTextField
+        name="Mot de passe"
+        type="text"
+        placeholder="Mot de passe"
+        validators={[
+          {
+            message: "La longueur de l'identifiant doit être compris entre 4 et 12 charactères.",
+            validator: value => validator.length(value, { min: 4, max: 12 }),
+          },
+        ]}
+        successMessage="Le mot de passe est invalide"
+        />
       </div>
     )
   }
